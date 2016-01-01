6279ea7ee35cbd4e19150c4a2da44b783ff26e00
Ruibin, Liu <lrb@pci-suntektech.com>
2016-01-01 22:15:53 +0800

    Finish function test case development.

--- app/src/androidTest/java/com/ruibin/actions/ActionsTest.java
+++ app/src/androidTest/java/com/ruibin/actions/ActionsTest.java
@@ -1,89 +1,147 @@
-/*
- * Copyright 2015, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.ruibin.actions;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.Until;
+
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import android.app.Activity;
-import android.support.test.espresso.action.ViewActions;
-import android.support.test.espresso.matcher.ViewMatchers;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.suitebuilder.annotation.LargeTest;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
-
-/**
- * Basic tests showcasing simple view matchers and actions like {@link ViewMatchers#withId},
- * {@link ViewActions#click} and {@link ViewActions#typeText}.
- * <p>
- * Note that there is no need to tell Espresso that a view is in a different {@link Activity}.
- */
 @RunWith(AndroidJUnit4.class)
-@LargeTest
+@SdkSuppress(minSdkVersion = 18)
 public class ActionsTest {
 
-    public static final String STRING_TO_BE_TYPED = "Espresso";
+    private static final String PACKAGE_NAME = "com.ruibin.actions";
 
-    /**
-     * A JUnit {@link Rule @Rule} to launch your activity under test. This is a replacement
-     * for {@link ActivityInstrumentationTestCase2}.
-     * <p>
-     * Rules are interceptors which are executed for each test method and will run before
-     * any of your setup code in the {@link Before @Before} method.
-     * <p>
-     * {@link ActivityTestRule} will create and launch of the activity for you and also expose
-     * the activity under test. To get a reference to the activity you can use
-     * the {@link ActivityTestRule#getActivity()} method.
-     */
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
-            MainActivity.class);
+    private static final int LAUNCH_TIMEOUT = 5000;
 
-    @Test
-    public void changeText_sameActivity() {
-        // Type text and then press the button.
-        onView(withId(R.id.editTextUserInput))
-                .perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
-        onView(withId(R.id.changeTextBt)).perform(click());
+    private static final String TITLE_TO_BE_TYPED = "Automatic Test";
+    private static final String DESCRIPTION_TO_BE_TYPED = "Verify add action function.";
 
-        // Check that the text was changed.
-        onView(withId(R.id.textToBeChanged)).check(matches(withText(STRING_TO_BE_TYPED)));
+    private UiDevice mDevice;
+
+    @Before
+    public void initialize() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        lunchMainActivityFromHomeScreen();
     }
 
     @Test
-    public void changeText_newActivity() {
-        // Type text and then press the button.
-        onView(withId(R.id.editTextUserInput)).perform(typeText(STRING_TO_BE_TYPED),
-                closeSoftKeyboard());
-        onView(withId(R.id.activityChangeTextBtn)).perform(click());
+    public void testActions_addAction() {
+        // Clear all action in database.
+        ActionController.getInstance().delete();
 
-        // This view is in a different Activity, no need to tell Espresso.
-        onView(withId(R.id.show_text_view)).check(matches(withText(STRING_TO_BE_TYPED)));
+        // Lunch the edit activity to create a new Action.
+        mDevice.findObject(By.res(PACKAGE_NAME, "action_new")).click();
+
+        // Wait for the activity to appear
+        mDevice.wait(Until.hasObject(By.pkg(PACKAGE_NAME).clazz(EditActivity.class)),
+                LAUNCH_TIMEOUT);
+
+        // Type in the title and description then press the 'save' button.
+        mDevice.findObject(By.res(PACKAGE_NAME, "title")).setText(TITLE_TO_BE_TYPED);
+        mDevice.findObject(By.res(PACKAGE_NAME, "description")).setText(DESCRIPTION_TO_BE_TYPED);
+        mDevice.findObject(By.res(PACKAGE_NAME, "action_save")).click();
+
+        // Wait for the activity to appear
+        mDevice.wait(Until.hasObject(By.pkg(PACKAGE_NAME).clazz(MainActivity.class)),
+                LAUNCH_TIMEOUT);
+
+        UiObject uiObject = mDevice.findObject(new UiSelector()
+                .className("android.support.v7.widget.RecyclerView")
+                .instance(0)
+                .childSelector(new UiSelector().className("android.widget.TextView")));
+
+        try {
+            assertTrue(TITLE_TO_BE_TYPED.equals(uiObject.getText()));
+        } catch (UiObjectNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testActions_achieveAction() {
+        UiObject uiObject = mDevice.findObject(new UiSelector()
+                .className("android.support.v7.widget.RecyclerView")
+                .instance(0)
+                .childSelector(new UiSelector().className("android.widget.CheckBox")));
+
+        try {
+            // Mark the action as 'achieved' status
+            uiObject.click();
+
+            assertTrue(uiObject.isChecked());
+        } catch (UiObjectNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Finish the activity, cause it to be destroy.
+        mDevice.pressBack();
+
+        lunchMainActivityFromHomeScreen();
+
+        uiObject = mDevice.findObject(new UiSelector()
+                .className("android.support.v7.widget.RecyclerView")
+                .instance(0)
+                .childSelector(new UiSelector().className("android.widget.CheckBox")));
+
+        try {
+            assertTrue(uiObject.isChecked());
+        } catch (UiObjectNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void lunchMainActivityFromHomeScreen() {
+        // Start from the home screen
+        mDevice.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+
+        // Launch the blueprint app
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager().getLaunchIntentForPackage(PACKAGE_NAME);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);    // Clear out any previous instances
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(PACKAGE_NAME).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    /**
+     * Uses package manager to find the package name of the device launcher. Usually this package
+     * is "com.android.launcher" but can be different at times. This is a generic solution which
+     * works on all platforms.`
+     */
+    private String getLauncherPackageName() {
+        // Create launcher Intent
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+
+        // Use PackageManager to get the launcher package name
+        PackageManager pm = InstrumentationRegistry.getContext().getPackageManager();
+        ResolveInfo resolveInfo = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return resolveInfo.activityInfo.packageName;
     }
 }
